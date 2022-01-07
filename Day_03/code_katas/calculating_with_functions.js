22ef91176c7dc49416d4b62de83fcfec73c9a398
jpromanonet <programming@jpromano.net>
2022-01-06 23:22:08 -0300

    Calculating with functions

--- Day_03/code_katas/calculating_with_functions.js
+++ Day_03/code_katas/calculating_with_functions.js
@@ -17,18 +17,43 @@ eight(dividedBy(three()));
 
 */
 
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+function expression(number, operation){
+    if(!operation)
+        return number;
+    return operation(number);
+}
 
-function plus() {}
-function minus() {}
-function times() {}
-function dividedBy() {}
+function zero(operation) { return expression(0, operation);}
+function one(operation) {return expression(1, operation);}
+function two(operation) {return expression(2, operation);}
+function three(operation) {return expression(3, operation);}
+function four(operation) {return expression(4, operation);}
+function five(operation) {return expression(5, operation);}
+function six(operation) {return expression(6, operation);}
+function seven(operation) {return expression(7, operation);}
+function eight(operation) {return expression(8, operation);}
+function nine(operation) {return expression(9, operation);}
+
+function plus(x){
+    return functrion(y){
+        return y + x;
+    }
+}
+
+function minux(x) {
+    return function(y) {
+        return y - x;
+    }
+}
+
+function times(x){
+    return function(y) {
+        return y * x;
+    }
+}
+
+function dividedBy(x){
+    return function(y) {
+        return y / x;
+    }
+}
